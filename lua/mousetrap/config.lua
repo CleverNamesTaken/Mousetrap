08368a19b7b0ddaa71473db1c7015640712c8d3d
root <root@localhost.localdomain>
2025-03-24 21:24:33 -0400

    Removed files from template; added documentation

--- lua/mousetrap/config.lua
+++ lua/mousetrap/config.lua
@@ -2,9 +2,6 @@
 ---@class Config
 local M = {}
 
----@class DefaultOptions
-M.defaults = { name = "John Doe" }
-
 ---@class Options
 -- need to use full paths, aliases or relative paths will not work
 local homeDir = os.getenv("HOME")
--- markdown/manual/docs/index.md
+++
@@ -1,4 +0,0 @@
-This website was generated automatically using
-a fancy Lua -> Markdown -> HTML translator. See
-[nvim-best-practices-plugin-template](https://github.com/ColinKennedy/nvim-best-practice
-s-plugin-template) for details!
